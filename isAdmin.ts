8ae05ae282a4c7c9acedccfa3e9cdded5397789b
Sasha Ryder <8694395+SashaRyder@users.noreply.github.com>
2024-02-07 00:36:43 +0000

    Incorrect check  for isAdmin

--- isAdmin.ts
+++ isAdmin.ts
@@ -3,7 +3,7 @@ import { Guild, GuildMember } from "discord.js"
 export const isAdmin = (user: GuildMember, guild: Guild): boolean => {
     const me = guild.members.me;
     const myHighestPermission = me.roles.highest.position;
-    const guildRoles = guild.roles.cache.filter((role) => role.position >= myHighestPermission);
+    const guildRoles = guild.roles.cache.filter((role) => role.position <= myHighestPermission);
     for (var role of guildRoles) {
         if (user.roles.cache.has(role[0])) {
             return true;
