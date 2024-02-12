903efc2a69a90dfe734d6f4f0239e99c1a2131e1
Sasha Ryder <8694395+SashaRyder@users.noreply.github.com>
2024-02-12 09:32:34 +0000

    added funny

---
+++ commands/rizz.ts
@@ -0,0 +1,21 @@
+import { CommandInterface } from "./CommandInterface";
+import { CommandInteraction, SlashCommandBuilder } from "discord.js";
+
+
+const execute = async (interaction: CommandInteraction) => {
+    const userToRizz = interaction.options.get("user").user;
+    await interaction.reply(`Hi ${userToRizz}
+    I am wobot
+    Beep book beep
+    :robot:
+    Beep beep
+    Friends uwu`);
+};
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("rizz")
+    .addUserOption((user) => user.setName("user").setDescription("User you want to rizz").setRequired(true))
+    .setDescription("Rizz command. Nuff said."),
+  execute,
+} as CommandInterface;
