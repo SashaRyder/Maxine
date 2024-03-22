d2a6be5d64f382dc967fee83b04f10e1c4d38e1d
Sasha Ryder <8694395+SashaRyder@users.noreply.github.com>
2024-03-22 19:50:52 +0000

    Added tldr command

--- commands/tldrify.ts
+++ commands/tldrify.ts
@@ -1,26 +1,27 @@
 import _ from "underscore";
 import { CommandInterface } from "./CommandInterface";
 import { ApplicationCommandType, CacheType, CommandInteraction, ContextMenuCommandBuilder, MessageContextMenuCommandInteraction } from "discord.js";
-import * as deepl from 'deepl-node';
-
+import { ChatGPTUnofficialProxyAPI } from 'chatgpt'
 
 const execute = async (interaction: CommandInteraction) => {
   if (!interaction.isMessageContextMenuCommand) return;
-  const authKey = process.env.DEEPL_API_KEY;
+  const authKey = process.env.CHATGPT_API_KEY;
   if(!authKey) {
-    await interaction.reply("No DeepL API key given");
+    await interaction.reply("No C-GPT API key given");
     return;
   };
 
   const msg = (interaction as MessageContextMenuCommandInteraction<CacheType>).targetMessage.content;
-  const translator = new deepl.Translator(authKey);
+  const api = new ChatGPTUnofficialProxyAPI({
+    accessToken: authKey
+  });
 
-  const result = await translator.translateText(msg, null, 'en-GB');
-  await interaction.reply(`${result.detectedSourceLang.toUpperCase()}: ${msg}\r\n\r\nEN: ${result.text}`);
+  const result = await api.sendMessage(`TLDR the following: ${msg}`);
+  await interaction.reply(result.text);
 };
 
 module.exports = {
   data: new ContextMenuCommandBuilder()
-    .setName("translate").setType(ApplicationCommandType.Message),
+    .setName("tldrify").setType(ApplicationCommandType.Message),
   execute,
 } as CommandInterface;
