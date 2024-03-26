42561c0fb610911ab8555d5cd163fedca462c279
Sasha Ryder <8694395+SashaRyder@users.noreply.github.com>
2024-03-26 15:26:53 +0000

    Added stderr for failed dls

--- downloader.ts
+++ downloader.ts
@@ -5,7 +5,7 @@ const { NICKNAME } = process.env;
 
 export const downloadVideo = async (url: string, showWarnings: boolean, isClip: boolean): Promise<string> => {
     const randomFileName = tmp.tmpNameSync({ prefix: NICKNAME });
-    const { stdout, exited } = Bun.spawn({
+    const { stdout, stderr, exited } = Bun.spawn({
         cmd: [
             `yt-dlp`,
             !showWarnings ? '--no-warnings' : '',
@@ -20,6 +20,10 @@ export const downloadVideo = async (url: string, showWarnings: boolean, isClip: 
     });
     const exitCode = await exited;
     const stdoutstr = await new Response(stdout).text();
+    const stderrstr = await new Response(stderr).text();
+    if(exitCode > 0 && stderrstr) {
+        throw new Error(stderrstr);
+    }
     const glob = new Glob(`**/${randomFileName.substring(randomFileName.lastIndexOf("/") + 1)}.*`);
     const filePath = Array.from(glob.scanSync({ absolute: true, cwd: "/tmp" }))[0];
     if (!await Bun.file(filePath).exists()) {
